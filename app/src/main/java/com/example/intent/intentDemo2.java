07033800c48c864c1e3b9e8e433cab7675ce70c4
unknown <752094571@qq.com>
2017-02-26 20:04:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/intent/intentDemo2.java
@@ -0,0 +1,18 @@
+package com.example.intent;
+
+/**
+ * Created by 金城 on 2017/2/26.
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class intentDemo2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intentdemo2);
+    }
+}
+
+
